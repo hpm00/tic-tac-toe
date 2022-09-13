fd58f007051e50cd2aef6ddfe1e1894bc1f3b25d
husna <hpm25@georgetown.edu>
2022-09-12 23:26:47 -0700

    fix spelling error line 5

--- player.rb
+++ player.rb
@@ -2,7 +2,9 @@
 class Player
     attr_reader :name, :character
     def initialize(name, character)
-      @name = name.captialize
+      @name = name.capitalize
       @character = character
     end 
 end
+
+# p player_1 = Player.new('brendan', 'X')
